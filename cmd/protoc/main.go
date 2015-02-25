9e8dac59ca2a3fc82cd0665ad32b1a36f3df40b8
Peter Mattis <petermattis@gmail.com>
2015-02-25 13:47:17 -0500

    Add support for protoc.
    Provide a small trampoline from the Go main() function down into the C
    main() function.

---
+++ cmd/protoc/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"os"
+	"unsafe"
+
+	_ "github.com/cockroachdb/c-protobuf"
+)
+
+// #cgo CXXFLAGS: -std=c++11
+// #cgo CPPFLAGS: -I../.. -I../../internal/src
+// #cgo darwin LDFLAGS: -Wl,-undefined -Wl,dynamic_lookup
+// #cgo !darwin LDFLAGS: -Wl,-unresolved-symbols=ignore-all
+//
+// void cmain(int argc, char **argv);
+import "C"
+
+func main() {
+	cargs := make([]*C.char, len(os.Args))
+	for i, a := range os.Args {
+		cargs[i] = C.CString(a)
+	}
+	C.cmain(C.int(len(os.Args)), (**C.char)(unsafe.Pointer(&cargs[0])))
+}
